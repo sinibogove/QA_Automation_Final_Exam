36ff49e8e1c1221d1e7598cc1155189cb43687d7
sinibogove <vdrangelov@gmail.com>
2022-04-07 20:21:55 +0300

    add CompleteOverviewPage

---
+++ src/test/java/pages/CompleteOrderPage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.Assert;
+
+import java.time.Duration;
+import java.util.Collections;
+
+public class CompleteOrderPage {
+    protected WebDriver driver;
+
+    @FindBy(xpath = "//*[text()='THANK YOU FOR YOUR ORDER']")
+    private WebElement thanksMessage;
+
+    public CompleteOrderPage (WebDriver driver){
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void thanksMessage (){
+
+        FluentWait fluentWait = new FluentWait(driver)
+                .withTimeout(Duration.ofSeconds(20))
+                .pollingEvery(Duration.ofSeconds(2)) //how often it will be checked for the presence of the element
+                .ignoreAll(Collections.singleton(NoSuchElementException.class));
+
+        fluentWait.until(ExpectedConditions.elementToBeClickable(thanksMessage));
+        Assert.assertTrue(thanksMessage.isDisplayed());
+    }
+}
